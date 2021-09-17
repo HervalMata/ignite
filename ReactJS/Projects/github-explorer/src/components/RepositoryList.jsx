2cc8e042f5b54873135f6208e3980de5ad57e399
Herval Mata <hervalmataamparo@gmail.com>
2021-09-17 14:58:46 -0300

    feat: art: github explorer - configured finished

---
+++ ReactJS/Projects/github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositório</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <a href="">Acessar repositório</a>
+                </li>
+            </ul>
+        </section>
+    );
+};
