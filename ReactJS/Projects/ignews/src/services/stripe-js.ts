9b6051c992030a93a14e08ea45f18189d49d3342
Herval Mata <hervalmataamparo@gmail.com>
2021-09-19 23:52:17 -0300

    feat: art: ignews - more stripe functions

---
+++ ReactJS/Projects/ignews/src/services/stripe-js.ts
@@ -0,0 +1,6 @@
+import {loadStripe} from "@stripe/stripe-js/pure";
+
+export async function getStripeJs() {
+    const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
+    return stripeJs;
+}
