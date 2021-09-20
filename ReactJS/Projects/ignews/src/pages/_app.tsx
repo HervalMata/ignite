3300a771f303ed917a3a8cf9e42ad6be480eb125
Herval Mata <hervalmataamparo@gmail.com>
2021-09-19 21:42:25 -0300

    feat: art: ignews - authentication and faunadb

--- ReactJS/Projects/ignews/src/pages/_app.tsx
+++ ReactJS/Projects/ignews/src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/global.scss'
 import {Header} from "../components/Header";
+import { Provider as NextAuthProvider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
   return (
-      <>
+      <NextAuthProvider session={pageProps.Session}>
         <Header />
         <Component {...pageProps} />
-      </>
+      </NextAuthProvider>
   )
 }
 
