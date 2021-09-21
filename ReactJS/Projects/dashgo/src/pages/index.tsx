ff133230a3ffb07e3862337e1acb5c3ef3adb851
Herval Mata <hervalmataamparo@gmail.com>
2021-09-20 23:07:56 -0300

    feat: art: dashgo - created comonent input

--- ReactJS/Projects/dashgo/src/pages/index.tsx
+++ ReactJS/Projects/dashgo/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import {Button, Flex, FormControl, FormLabel, Input, Stack} from "@chakra-ui/react";
+import {Button, Flex, Stack} from "@chakra-ui/react";
+import { Input} from "../components/Form/Input";
 
 export default function Home() {
   return (
@@ -7,22 +8,12 @@ export default function Home() {
           <Flex as="form" width="100%" maxWidth={360} bg='gray.800'
                 p='8' borderRadius='8' flexDir="column">
               <Stack spacing='4'>
-                  <FormControl>
-                      <FormLabel htmlFor="email">Email</FormLabel>
-                      <Input type='email' name='email'
-                             focusBorderColor='pink.500'
-                             bgColor="gray.900" variant='filled'
-                             _hover={{ bgColor: 'gray.900' }}
-                             size='lg' id='email'
-                      />
-                      <FormLabel htmlFor="password">Senha</FormLabel>
-                      <Input type='password' name='password'
-                             focusBorderColor='pink.500'
-                             bgColor="gray.900" variant='filled'
-                             _hover={{ bgColor: 'gray.900' }}
-                             size='lg' id='password'
-                      />
-                  </FormControl>
+                  <Input type='email' name='email'
+                             label='Email'
+                  />
+                  <Input type='password' name='password'
+                         label='Senha'
+                  />
               </Stack>
               <Button type="submit" mt="6" colorScheme='pink'>
                   Entrar
