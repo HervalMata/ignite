fb3c9e7695667fcdb281bdb75b7ca8be70c8c7ea
Herval Mata <hervalmataamparo@gmail.com>
2021-09-21 17:14:52 -0300

    feat: art: dashgo - created dashboard

--- ReactJS/Projects/dashgo/src/components/Header.tsx
+++ ReactJS/Projects/dashgo/src/components/Header.tsx
@@ -9,19 +9,20 @@ export function Header() {
             as='header' w='100%' maxWidth={1480}
             h="28" marginX='auto' mt='4' px='6' align='center'>
             <Text
-                fontSize="3x1" fontHeight="bold"
+                fontSize="3x1" fontWeight="bold"
                 letterSpacing="light" w="64"
             >
                 dashgo
+                <Text as='span' ml='1' color="pink.500">.</Text>
             </Text>
-            <Text as='span' ml='1' color="pink.500">.</Text>
+
             <Flex as='label' flex='1' py='4' px='8'
                   ml='6' maxWidth='400' alignSelf='center'
                   color='gray.200' position='relative'
                   bg='gray.800' borderRadius='full'>
                 <Input
                     color='gray.50' variant='unstyled'
-                    py='4' mr='4' placeholder='Buscar na plataforma'
+                    px='4' mr='4' placeholder='Buscar na plataforma'
                     _placeholder={{ color: 'gray.400 '}} />
                 <Icon as={RiSearchLine} fontSize='20' />
             </Flex>
