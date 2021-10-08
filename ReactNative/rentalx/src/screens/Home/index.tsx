9b55220c62b245a5267d150c0aa62ede6982e739
Herval Mata <hervalmataamparo@gmail.com>
2021-10-07 23:00:47 -0300

    feat: art: rn rentalx - add card car component

--- ReactNative/rentalx/src/screens/Home/index.tsx
+++ ReactNative/rentalx/src/screens/Home/index.tsx
@@ -4,6 +4,7 @@ import Logo from "../../assets/logo.svg";
 import {RFValue} from "react-native-responsive-fontsize";
 
 import * as S from './styles'
+import {CardCar} from "../../components";
 
 export function Home() {
     return (
@@ -20,6 +21,16 @@ export function Home() {
                     <S.TotalCar>Total de 12 carros</S.TotalCar>
                 </S.HeaderContent>
             </S.Header>
+            <S.CardList data={[1,2,3,4,5,6]}
+                        keyExtractor={item => String(item)}
+                        renderItem={() => (
+                            <CardCar data={{
+                                brand: 'Audi',
+                                name: 'RS S Coupé',
+                                rent: { period: 'Ao dia', price: 120},
+                                thumbnail: 'https://e7.pngegg.com/pngimages/889/300/png-clipart-audi-sportback-concept-car-audi-a3-2018-audi-a5-coupe-audi-compact-car-sedan.png'
+                            }} />
+                        )} />
         </S.Container>
     )
 }
